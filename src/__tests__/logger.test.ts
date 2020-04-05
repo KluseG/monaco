fe52e5f9177ae5c4012984781f51633a4712b597
Adrian Kluska <adrian.kluska@movecloser.pl>
2020-04-05 15:28:26 +0200

    Fixed test suite

--- src/__tests__/logger.test.ts
+++ src/__tests__/logger.test.ts
@@ -2,33 +2,60 @@ import Monaco from '../index';
 import { LogMachine } from '../log-machine';
 import { ForegroundColors, BackgroundColors } from '../contracts/enums';
 
-/**
- * error
- * info
- * notice
- * success
- * warn
- */
-
 test('Returns correct LogMachine instance', () => {
   const logger = new Monaco();
   expect(logger.error('')).toStrictEqual(
-    new LogMachine(0, null, { fg: ForegroundColors.error, bg: BackgroundColors.error }, 'error', [''])
+    new LogMachine(
+      0,
+      null,
+      { fg: ForegroundColors.error, bg: BackgroundColors.error },
+      { fg: ForegroundColors.primary, bg: BackgroundColors.primary },
+      'error',
+      ['']
+    )
   );
 
   expect(logger.info('')).toStrictEqual(
-    new LogMachine(0, null, { fg: ForegroundColors.info, bg: BackgroundColors.info }, 'info', [''])
+    new LogMachine(
+      0,
+      null,
+      { fg: ForegroundColors.info, bg: BackgroundColors.info },
+      { fg: ForegroundColors.primary, bg: BackgroundColors.primary },
+      'info',
+      ['']
+    )
   );
 
   expect(logger.notice('')).toStrictEqual(
-    new LogMachine(0, null, { fg: ForegroundColors.notice, bg: BackgroundColors.notice }, 'notice', [''])
+    new LogMachine(
+      0,
+      null,
+      { fg: ForegroundColors.notice, bg: BackgroundColors.notice },
+      { fg: ForegroundColors.primary, bg: BackgroundColors.primary },
+      'notice',
+      ['']
+    )
   );
 
   expect(logger.success('')).toStrictEqual(
-    new LogMachine(0, null, { fg: ForegroundColors.success, bg: BackgroundColors.success }, 'success', [''])
+    new LogMachine(
+      0,
+      null,
+      { fg: ForegroundColors.success, bg: BackgroundColors.success },
+      { fg: ForegroundColors.primary, bg: BackgroundColors.primary },
+      'success',
+      ['']
+    )
   );
 
   expect(logger.warn('')).toStrictEqual(
-    new LogMachine(0, null, { fg: ForegroundColors.warn, bg: BackgroundColors.warn }, 'warn', [''])
+    new LogMachine(
+      0,
+      null,
+      { fg: ForegroundColors.warn, bg: BackgroundColors.warn },
+      { fg: ForegroundColors.primary, bg: BackgroundColors.primary },
+      'warn',
+      ['']
+    )
   );
 });
